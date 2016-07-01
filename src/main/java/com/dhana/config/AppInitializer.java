d35ea1f0066c81d32d3f06166d82b647f5779254
Dhanabalan <dhanabalan.java@gmail.com>
2016-07-01 18:33:39 +0530

    Java based configuration

--- src/main/java/com/dhana/config/AppInitializer.java
+++ src/main/java/com/dhana/config/AppInitializer.java
@@ -3,9 +3,13 @@
  */
 package com.dhana.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import com.dhana.Application;
 
@@ -19,4 +23,14 @@ public class AppInitializer extends SpringBootServletInitializer {
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(Application.class);
 	}
+	
+	@Bean
+	 public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://127.3.175.2:3306/sivam");
+        dataSource.setUsername("adminSU4sCUv");
+        dataSource.setPassword("ihb-uiahj564");
+        return dataSource;
+    }
 }
